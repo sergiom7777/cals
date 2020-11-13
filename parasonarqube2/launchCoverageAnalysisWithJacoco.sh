f6d79bddd778b705d9edfc64676904cabff38118
sergiom7777 <62477362+sergiom7777@users.noreply.github.com>
2020-11-13 18:37:05 +0100

    Update launchCoverageAnalysisWithJacoco.sh

--- parasonarqube2/launchCoverageAnalysisWithJacoco.sh
+++ parasonarqube2/launchCoverageAnalysisWithJacoco.sh
@@ -1,3 +1,4 @@
 mvn clean compile test-compile;
 mvn -Djacoco.destFile=target/coverage/jacoco.exec org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test;
 mvn -X -Djacoco.dataFile=target/coverage/jacoco.exec -Djacoco.outputDirectory=./jacoco_ut org.jacoco:jacoco-maven-plugin:report
+#aasasas
